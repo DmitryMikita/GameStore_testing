252943aa25c559003f6f2bfaf1829d80a07333fb
dmitrymikita <wmists@gmail.com>
2022-08-11 02:03:13 +1000

    Add tests

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
@@ -14,5 +15,110 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    @Test
+    //Не должна создаваться игра с уже существующими заголовком и жанром
+    public void shouldAddExistingGame() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        assertFalse(store.containsGame(game2));
+    }
+
+    @Test
+    // не должны добавляться в каталог игры без названия и жанра
+    public void shouldAddNotTitleGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame(null, null);
+
+
+        assertFalse(store.containsGame(game));
+    }
+
+    @Test
+    //
+    public void shouldAddPlayerTime() {
+
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 1);
+
+        String actual = store.getMostPlayer();
+        String expected = "Ivan";
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    //общее время игрока не может быть отрицательным
+    public void shouldAddNegativeTime() {
+
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", -4);
+
+        String actual = store.getMostPlayer();
+        String expected = null;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //время игрока должно суммироваться c прошлым значением
+    public void shouldSumPlayerTime() {
+
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 6);
+        store.addPlayTime("Ivan", 4);
+        store.addPlayTime("Fill", 8);
+        String actual = store.getMostPlayer();
+        String expected = "Ivan";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //должен показываться игрок с наибольшим временeм в игре
+    public void shouldMostPlayer() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("John", 5);
+        store.addPlayTime("Ivan", 1);
+        store.addPlayTime("Fill", 2);
+        String actual = store.getMostPlayer();
+        String expected = "John";
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    //если у всех игроков равное время игры, то при вызове игрока с большим временем, должен возвращаться первый
+    public void shouldMostPlayerEqualTime() {
+
+        GameStore store = new GameStore();
+        store.addPlayTime("Fill", 5);
+        store.addPlayTime("Ivan", 5);
+        store.addPlayTime("John", 5);
+        String actual = store.getMostPlayer();
+        String expected = "Fill";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+//метод getSumPlayedTime должен возвращать количество времени всех игроков
+    public void shouldSumPlayedTime() {
+
+        GameStore store = new GameStore();
+        store.addPlayTime("Fill", 5);
+        store.addPlayTime("Ivan", 5);
+        store.addPlayTime("John", 5);
+        int actual = store.getSumPlayedTime();
+        int expected = 15;
+
+        assertEquals(expected, actual);
+    }
+
+
 }
